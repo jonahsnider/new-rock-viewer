90dfc0c368ae2231d29edb4703b1c5cdb80bb265
Jonah Snider <jonah@jonahsnider.com>
2025-11-24 21:50:42 -0800

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'New Rock',
+
+  projectId: '5zgry73m',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
